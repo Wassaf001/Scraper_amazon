e458f911c689939be50deb1a385d5c2eac5a1e26
Stuck <98597682+Wassaf001@users.noreply.github.com>
2023-06-03 13:09:28 +0530

    scrape_amazon.py
    Scraper built to extract data of smartphones under price of 20,000 rupees.

--- scrape_amazon.py
+++ scrape_amazon.py
@@ -2,8 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_amazon():
-    url = "https://www.tripadvisor.in/"
-    response = requests.get(url)
+    url = "https://www.amazon.in/s?k=smartphones&rh=p_36%3A-2000000"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
 
     results = soup.find_all("div", {"data-component-type": "s-search-result"})
@@ -21,5 +24,4 @@ def scrape_amazon():
             print("---------------------")
 
 scrape_amazon()
-
 
